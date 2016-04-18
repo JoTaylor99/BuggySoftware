76fe353c8d907b494d2febbcecec43a579214172
HBromell <hb12762@bristol.ac.uk>
2016-04-18 02:01:35 +0100

    Made turning condition more strict (now works :D)

--- navigation.cpp
+++ navigation.cpp
@@ -473,7 +473,7 @@ void navigation::adjustOnTheSpot(){
 void navigation::turnLeft() {
 	while (true) {
 		Sensor::PollSensors(Sensors);
-		if (RVAL(sC::LTL) != STARTVAL(sC::LTL)) {
+		if ((RVAL(sC::LTL) != STARTVAL(sC::LTL)) && (RVAL(sC::BR) != STARTVAL(sC::BR))) {
 			drive(nC::Direction::Stop);
 			break;
 		}
@@ -509,7 +509,7 @@ void navigation::turnRight() {
 	drive(nC::Direction::Right);
 	while (true) {
 		Sensor::PollSensors(Sensors);
-		if (RVAL(sC::LTR) != STARTVAL(sC::LTR)) {
+		if ((RVAL(sC::LTR) != STARTVAL(sC::LTR)) && (RVAL(sC::BL) != STARTVAL(sC::BL))) {
 			drive(nC::Direction::Stop);
 			break;
 		}
