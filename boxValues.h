3419e44227dd6c8cd4119f4b42975442f05daf86
My name is alex <ad13306@my.bristol.ac.uk>
2016-04-20 22:58:42 +0100

    box analysis order for will

--- boxValues.h
+++ boxValues.h
@@ -9,9 +9,28 @@ namespace boxValues{
 		double c1;
 		double f;
 	};
-
-
-
+	/* DC for WIll
+	box 1 
+	voltage at p2 multiplied by ratio
+	box 2
+	stage 1 Ra 
+	stage 2 Rb
+	box 3
+	stage 1 Ra
+	stage 2 Rb
+	box 4
+	stage 1 Ra
+	stage 2 Rb
+	box 5
+	stage 1 R
+	stage 2 C
+	box 6 
+	stage 1 R
+	stage 2 C
+	box 7
+	stage 1 R
+	stage 2 C
+	*/
 
 
 
